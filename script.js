68581c81e5991d735d2662ee38661d65adec1598
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-10 13:21:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(specis){
+		this.specis = specis;
+	}
+	Getter(){
+		return this.specis;
+	}
+	makeSound(){
+		return `The ${this.specis} make a sound`;
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+		console.log("wool");
+	}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
